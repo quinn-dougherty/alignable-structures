ff9a96c6488f3b959364ccfef9dacd73ec4d0b63
Quinn Dougherty <quinnd@riseup.net>
2022-11-07 18:08:00 -0500

    rm constraint that we don't post the docs site anywhere

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,64 +1,51 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Do not share',
-    Svg: require('@site/static/img/ideogram.svg').default,
+    title: "Funded",
+    Svg: require("@site/static/img/ideogram.svg").default,
+    description: (
+      <>Food, flights, airbnb covered by the Long Term Future Fund</>
+    ),
+  },
+  {
+    title: "Please submit your session by October 1st",
+    Svg: require("@site/static/img/ideogram.svg").default,
     description: (
       <>
-        By invitation only. Docs site is publicly listed, but we do not want it posted anywhere.
+        We'd like the schedule to cool down around the first week of october,
+        subject to changes and exceptions.
       </>
     ),
   },
   {
-    title: 'Funded',
-    Svg: require('@site/static/img/ideogram.svg').default,
+    title: "Discord for updates",
+    Svg: require("@site/static/img/ideogram.svg").default,
     description: (
       <>
-        Food, flights, airbnb covered by the Long Term Future Fund
+        You should have received an invite link to the discord. It is the main
+        communication channel for the workshop, please join it.
       </>
     ),
   },
   {
-    title: 'Please submit your session by October 1st',
-    Svg: require('@site/static/img/ideogram.svg').default,
-    description: (
-      <>
-        We'd like the schedule to cool down around the first week of october, subject to changes and exceptions.
-      </>
-    ),
-  },
-  {
-    title: 'Discord for updates',
-    Svg: require('@site/static/img/ideogram.svg').default,
-    description: (
-      <>
-        You should have received an invite link to the discord. It is the main communication channel for the workshop, please join it.
-      </>
-    ),
-  },
-  {
-    title: 'Google calendar for schedule',
-    Svg: require('@site/static/img/ideogram.svg').default,
-    description: (
-      <>
-        You can find a link in the discord.
-      </>
-    ),
+    title: "Google calendar for schedule",
+    Svg: require("@site/static/img/ideogram.svg").default,
+    description: <>You can find a link in the discord.</>,
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
